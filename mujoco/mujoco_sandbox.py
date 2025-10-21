e00d7c4aa307d3f4d633a785a68ad10bb33eb1f1
Mohid Khan <mohidkhan169@gmail.com>
2025-10-20 23:34:15 -0700

    rendering sphere

--- mujoco/mujoco_sandbox.py
+++
@@ -1,9 +0,0 @@
-import mujoco, mujoco.viewer.viewer
-from mujoco import MjModel, MjData, MjViewer
-
-model = MjModel.from_file("assets/bouncing_ball.xml")
-data = MjData(model)
-viewer = MjViewer(model, data)
-
-while True:
-    viewer.render()
---
+++ mujoco/render_sphere.py
@@ -0,0 +1,11 @@
+import mujoco, mujoco.viewer
+from mujoco import MjModel, MjData
+
+model = MjModel.from_xml_string("<mujoco><worldbody><geom type='sphere' size='0.05'/></worldbody></mujoco>")
+data  = MjData(model)
+
+viewer = mujoco.viewer.launch_passive(model, data)
+
+while viewer.is_running():
+    mujoco.mj_step(model, data)
+    viewer.sync()
