6447eab56f32fdb37f484bb3ee39eb40ee4ec4f0
Mohid Khan <mohidkhan169@gmail.com>
2025-10-21 22:45:41 -0700

    linting

--- mujoco/arm_movement.py
+++ mujoco/arm_movement.py
@@ -1,5 +1,7 @@
 # run_arm.py
-import os; os.environ["MUJOCO_GL"] = "glfw"  # macOS-friendly
+import os
+
+os.environ["MUJOCO_GL"] = "glfw"  # macOS-friendly
 import numpy as np
 import mujoco, mujoco.viewer
 from mujoco import MjModel, MjData
@@ -14,6 +16,6 @@ viewer = mujoco.viewer.launch_passive(m, d)
 while viewer.is_running():
     for i in range(1000):
         mujoco.mj_step(m, d)
-        d.ctrl[0] = 0.8*np.sin(2*np.pi*0.5*d.time)
-        d.ctrl[1] = 0.6*np.sin(2*np.pi*0.8*d.time+1.0)
+        d.ctrl[0] = 0.8 * np.sin(2 * np.pi * 0.5 * d.time)
+        d.ctrl[1] = 0.6 * np.sin(2 * np.pi * 0.8 * d.time + 1.0)
         viewer.sync()
