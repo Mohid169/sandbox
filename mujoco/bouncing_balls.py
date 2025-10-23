9d88671afc090c9db30102ba1c0f483124326ad5
Mohid Khan <mohidkhan169@gmail.com>
2025-10-22 22:23:33 -0700

    bouncing ball

---
+++ mujoco/bouncing_balls.py
@@ -0,0 +1,13 @@
+import os; os.environ["MUJOCO_GL"]="glfw"
+import mujoco, mujoco.viewer
+from mujoco import MjModel, MjData
+
+m = MjModel.from_xml_path("assets/bouncing_balls.xml")
+d = MjData(m)
+
+viewer = mujoco.viewer.launch_passive(m, d)
+print("Close window to stop.")
+while viewer.is_running():
+    mujoco.mj_step(m, d)
+    viewer.sync()
+viewer.close()
