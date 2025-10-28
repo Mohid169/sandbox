dc8114d119e97957fb3793d28ee62d75dd605409
Mohid Khan <mohidkhan169@gmail.com>
2025-10-27 23:24:22 -0700

    adding cart_pole

---
+++ mujoco/cart_pole.py
@@ -0,0 +1,12 @@
+import mujoco
+from mujoco import viewer
+m = mujoco.MjModel.from_xml_path("assets/cartpole.xml")   # point to the file from #1
+d = mujoco.MjData(m)
+
+viewer = mujoco.viewer.launch_passive(m, d)
+
+while viewer.is_running():
+    for _ in range(1000):   
+        mujoco.mj_step(m, d)
+        d.ctrl[0] = 1.0
+        viewer.sync()
